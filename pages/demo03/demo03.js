cff3f8d9354fe65eb436495035a7d0af52aeffd3
anxiaofeng <1263606186@qq.com>
2020-06-30 18:02:04 +0800

    数据的绑定

---
+++ pages/demo03/demo03.js
@@ -0,0 +1,20 @@
+// pages/demo03/demo03.js
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    msg: 'hello mina',
+    num: 10000,
+    isGirl: false,
+    person: {
+      name: "富婆",
+      age: '80',
+      height: 174,
+      weight: 200
+    },
+    isChecked: false
+  },
+
+ 
+})
